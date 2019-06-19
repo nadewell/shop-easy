0c773201d5d74b47458dfabeb4a89f834129f445
Nishant Shaligram <nishant.shaligram@gmail.com>
2019-06-19 23:09:26 +0530

    index page skelton ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
-jQuery('.hero-slider').slick({
+jQuery('.hero-slider .slider').slick({
     arrows: true,
-    dots: true,
+    dots: false,
     prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
     nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
     autoplay: true,
     autoplaySpeed: 3000,
+});
+jQuery('.featured-products .slider').slick({
+    arrows: true,
+    dots: false,
+    prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+    nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
 });
 jQuery('section.testimonials .testimonials-wrapper').slick({
     arrows: true,
